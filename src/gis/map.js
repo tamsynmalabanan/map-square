5a9669d12169d11ab845a308b8225f6ba4bb6c54
tamsynmalabanan <tamsyn.malabanan@gmail.com>
2026-07-08 16:32:47 +0530

    config maplibregl.Map extension

--- src/gis/map.js
+++ src/gis/map.js
@@ -4,135 +4,6 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 import * as utils from '../utils.js'; 
 import * as gisUtils from './utils.js'; 
 
-const defaultSettings = {
-  locked: true,
-  unit: 'metric',
-  precision: 1000000,
-  projection: 'mercator',
-  terrain: false,
-  bookmark: {
-    extents: [{
-      active: true,
-      name: 'centroid',
-      title: 'Centroid',
-      params: {
-        zoom: 1,
-        lng: 0,
-        lat: 3,
-      },
-    }, {
-      active: false,
-      name: 'bbox',
-      title: 'Bounding Box',
-      params: {
-        w: -140,
-        s: -70,
-        e: 160,
-        n: 90,
-        padding: 0,
-        maxZoom: 1,
-      }
-    }],
-    pitch: 0,
-    bearing: 0,
-  },  
-  basemap: {
-    render: true,
-    theme: 'auto',
-    paints: {
-      default: {
-        basemap: {
-          'raster-resampling': 'linear',
-          'raster-opacity': 1,
-          'raster-hue-rotate': 0,
-          'raster-brightness-min': 0,
-          'raster-brightness-max': 1,
-          'raster-saturation': 0,
-          'raster-contrast': 0,
-        },
-        sky: {
-          "sky-color": "#88c6fc",
-          "horizon-color": "#ffffff",
-          "fog-color": "#ffffff",
-          "fog-ground-blend": 0.5,
-          "horizon-fog-blend": 0.8,
-          "sky-horizon-blend": 0.8,
-          "atmosphere-blend": 0.8
-        },
-      },
-      dark: {
-        basemap: {
-          'raster-resampling': 'linear',
-          'raster-opacity': 1, // 0 to 1
-          'raster-hue-rotate': 0, // 0 to 360
-          'raster-brightness-min': 0, // 0 to 1
-          'raster-brightness-max': 0.005, // 0 to 1
-          'raster-saturation': -1, // -1 to 1
-          'raster-contrast': 0.995, // -1 to 1
-        },
-        sky: {
-          "sky-color": "#02294b",
-          "horizon-color": "#808080",
-          "fog-color": "#808080",
-          "fog-ground-blend": 0.5,
-          "horizon-fog-blend": 0.8,
-          "sky-horizon-blend": 0.8,
-          "atmosphere-blend": 0.8
-        }
-      },
-    }
-  },
-  hillshade: {
-    render: true,
-    methods: [{
-      active: true,
-      name: 'standard',
-      title: 'Standard',
-      params: {
-        'hillshade-illumination-direction': 315,
-        'hillshade-illumination-altitude': 45,
-        'hillshade-highlight-color': '#FFFFFF',
-        'hillshade-shadow-color': '#000000',
-      }
-    }, {
-      active: false,
-      name: 'multi',
-      title: 'Multidirectional',
-      params: {
-        'hillshade-illumination-direction': [315, 45, 135, 225],
-        'hillshade-illumination-altitude': [45, 45, 45, 45],
-        'hillshade-highlight-color': [
-          '#ff0000',
-          '#80ff00',
-          '#00ffff',
-          '#7f00ff',
-        ],
-        'hillshade-shadow-color': [
-          '#503030',
-          '#405030',
-          '#305050',
-          '#403050',
-        ],
-      }
-    }],
-    exaggeration: 0.1,
-    accent: '#000000',
-  },
-  interactions: {
-    tooltip: {
-      active: true,
-    },
-    popup: {
-      active: true,
-      targets: {
-        layers: true,
-        osm: true,
-        elevation: true,
-      }
-    }
-  },
-}
-
 const defaultConfig = {
   sources: {
     basemap: {
@@ -153,7 +24,134 @@ const defaultConfig = {
   themes: [{
     active: true,
     id: utils.randomId(),
-    settings: structuredClone(defaultSettings),
+    settings: {
+      locked: true,
+      unit: 'metric',
+      precision: 1000000,
+      projection: 'mercator',
+      terrain: false,
+      bookmark: {
+        extents: [{
+          active: true,
+          name: 'centroid',
+          title: 'Centroid',
+          params: {
+            zoom: 1,
+            lng: 0,
+            lat: 3,
+          },
+        }, {
+          active: false,
+          name: 'bbox',
+          title: 'Bounding Box',
+          params: {
+            w: -140,
+            s: -70,
+            e: 160,
+            n: 90,
+            padding: 0,
+            maxZoom: 1,
+          }
+        }],
+        pitch: 0,
+        bearing: 0,
+      },  
+      basemap: {
+        render: true,
+        theme: 'auto',
+        paints: {
+          default: {
+            basemap: {
+              'raster-resampling': 'linear',
+              'raster-opacity': 1,
+              'raster-hue-rotate': 0,
+              'raster-brightness-min': 0,
+              'raster-brightness-max': 1,
+              'raster-saturation': 0,
+              'raster-contrast': 0,
+            },
+            sky: {
+              "sky-color": "#88c6fc",
+              "horizon-color": "#ffffff",
+              "fog-color": "#ffffff",
+              "fog-ground-blend": 0.5,
+              "horizon-fog-blend": 0.8,
+              "sky-horizon-blend": 0.8,
+              "atmosphere-blend": 0.8
+            },
+          },
+          dark: {
+            basemap: {
+              'raster-resampling': 'linear',
+              'raster-opacity': 1, // 0 to 1
+              'raster-hue-rotate': 0, // 0 to 360
+              'raster-brightness-min': 0, // 0 to 1
+              'raster-brightness-max': 0.005, // 0 to 1
+              'raster-saturation': -1, // -1 to 1
+              'raster-contrast': 0.995, // -1 to 1
+            },
+            sky: {
+              "sky-color": "#02294b",
+              "horizon-color": "#808080",
+              "fog-color": "#808080",
+              "fog-ground-blend": 0.5,
+              "horizon-fog-blend": 0.8,
+              "sky-horizon-blend": 0.8,
+              "atmosphere-blend": 0.8
+            }
+          },
+        }
+      },
+      hillshade: {
+        render: true,
+        methods: [{
+          active: true,
+          name: 'standard',
+          title: 'Standard',
+          params: {
+            'hillshade-illumination-direction': 315,
+            'hillshade-illumination-altitude': 45,
+            'hillshade-highlight-color': '#FFFFFF',
+            'hillshade-shadow-color': '#000000',
+          }
+        }, {
+          active: false,
+          name: 'multi',
+          title: 'Multidirectional',
+          params: {
+            'hillshade-illumination-direction': [315, 45, 135, 225],
+            'hillshade-illumination-altitude': [45, 45, 45, 45],
+            'hillshade-highlight-color': [
+              '#ff0000',
+              '#80ff00',
+              '#00ffff',
+              '#7f00ff',
+            ],
+            'hillshade-shadow-color': [
+              '#503030',
+              '#405030',
+              '#305050',
+              '#403050',
+            ],
+          }
+        }],
+        exaggeration: 0.1,
+        accent: '#000000',
+      },
+      interactions: {
+        tooltip: {
+          active: true,
+        },
+        popup: {
+          active: true,
+          targets: {
+            layers: true,
+            osm: true,
+            elevation: true,
+          }
+        }
+      },
+    },
     metadata: {
       title: 'Untitled Map',
       abstract: '',
@@ -195,37 +193,74 @@ const defaultConfig = {
 }
 
 export default class Map extends maplibregl.Map {
-  constructor(container, config) {
-    if (!config) {
-      config = structuredClone(defaultConfig)
+  constructor(container, config=null) {
+    const cloneConfig = structuredClone(defaultConfig)
+    const cloneTheme = cloneConfig.themes[0]
+    const cloneSettings = cloneTheme.settings
+    const cloneCentroid = cloneSettings.bookmark.extents.find(props => props.name == 'centroid')
+
+    config ??= cloneConfig
+
+    let theme = (config.themes ??= []).find(theme => theme.active)
+    if (!theme) {
+      theme = config.themes[0] ??= cloneTheme
+      theme.active = true
     }
 
-    const theme = config.themes.find(theme => theme.active)
-    const settings = theme.settings
+    const settings = theme.settings ??= cloneSettings
+    const bookmark = settings.bookmark ??= cloneSettings.bookmark
+    
+    let extent = (bookmark.extents ??= cloneSettings.bookmark.extents).find(props => props.active)
+    if (!extent) {
+      extent = bookmark.extents[0] ??= cloneSettings.bookmark.extents[0]
+      extent.active = true
 
-    const bookmark = settings.bookmark
-    const extent = bookmark.extents.find(props => props.active)
+      const extentNames = bookmark.extents.map(props => props.name)
+      cloneSettings.bookmark.extents.forEach(props => {
+        if (!extentNames.includes(props.name)) {
+          props.active = false
+          bookmark.extents.push(props)
+        }
+      })
+    }
 
-    const basemap = settings.basemap
-    const paints = basemap.paints[(basemap.theme == 'dark' || (
+    const basemap = settings.basemap ??= cloneSettings.basemap
+    basemap.render ??= cloneSettings.basemap.render
+    basemap.theme ??= cloneSettings.basemap.theme
+    
+    let paints = (basemap.paints ??= cloneSettings.basemap.paints)[(basemap.theme == 'dark' || (
       basemap.theme == 'auto' && Alpine.store('displaySettings').darkModeIsOn
     )) ? 'dark' : 'default']
-    
+    if (!paints) {
+      basemap.theme = Object.keys(basemap.paints)[0] ?? Object.keys(cloneSettings.basemap.paints)[0]
+      paints = basemap.paints[basemap.theme] ??= cloneSettings.basemap.paints[basemap.theme]
+
+      Object.entries(cloneSettings.basemap.paints).forEach(([name, props]) => {
+        if (!(name in basemap.paints)) {
+          basemap.paints[name] = props
+        }
+      })
+    }
+
+    const sources = config.sources ??= cloneConfig.sources
+    sources.basemap ??= cloneConfig.sources.basemap
+    sources.terrain ??= cloneConfig.sources.terrain
+
     const options = {
       container,
-      pitch: bookmark.pitch,
-      bearing: bookmark.bearing,
+      pitch: bookmark.pitch ??= cloneSettings.bookmark.pitch,
+      bearing: bookmark.bearing ??= cloneSettings.bookmark.bearing,
       ...(extent.name == 'centroid' ? {
-        zoom: extent.params.zoom,
-        center: Array('lng', 'lat').map(i => extent.params[i]),
+        zoom: extent.params.zoom ??= cloneCentroid.params.zoom,
+        center: Array('lng', 'lat').map(i => extent.params[i] ??= cloneCentroid.params[i]),
       } : {}),
       maxZoom: 22,
       maxPitch: 75,
-      interactive: !settings.locked,
+      interactive: !(settings.locked ??= cloneSettings.locked),
       hash: false,
       style: {
         version: 8,
-        sources: config.sources,
+        sources,
         ...(basemap.render ? {
           layers: [{
             id: 'basemap',
@@ -239,7 +274,11 @@ export default class Map extends maplibregl.Map {
     }
 
     super(options);
-    
+
+    this.on('load', () => {
+
+    })
+
     this.config = config
     this.theme = theme
 
@@ -250,6 +289,10 @@ export default class Map extends maplibregl.Map {
     window.map = this
   }
   
+  static normalizeConfig() {
+
+  }
+
   configAddLayer() {
     const originalAddLayer = this.addLayer.bind(this)
 
